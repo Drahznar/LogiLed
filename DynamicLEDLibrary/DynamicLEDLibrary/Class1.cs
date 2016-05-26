05b4a6a6dda120e29f3b83a50b6c255e34b5bd67
Kevin Pirmer <k.3vin@live.de>
2016-05-26 16:48:12 +0200

    First steps

---
+++ DynamicLEDLibrary/DynamicLEDLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using LedCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLEDLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ DynamicLEDLibrary/DynamicLEDLibrary/HelperClass.cs
@@ -0,0 +1,17 @@
+﻿using LedCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLEDLibrary
+{
+    class HelperClass : LogiLed
+    {
+        protected void InitLed()
+        {
+            InitLed();
+        }
+    }
+}
